1640cfda908f720d9e36c7a80b504c54e4b3b14c
Raffaell Negreiros Guimaraes <raffaellng18@gmail.com>
2024-02-06 19:04:45 -0300

    Teste simples e basico com TDD

---
+++ CursoOnline.Domain.Test/Cursos/CursoTest.cs
@@ -0,0 +1,45 @@
+﻿using CursoOnline.Domain.Curso.Enum;
+using Xunit;
+
+namespace CursoOnline.Domain.Test.Cursos
+{
+    public class CursoTest
+    {
+        //Criar um curso com os seguintes dados:
+        //Nome, carga horaria, publico, valor curso
+
+        [Fact]
+        public void Curso_CriarCurso_RetornaCurso_Green()
+        {
+            var CursoEsperado = new
+            {
+                Nome = "Curso de TDD",
+                CargaHoraria = (double)40,
+                PublicoAlvo = PublicAlvo.Empresarial,
+                Valor = (decimal)100
+            };
+
+            var curso = new Curso.Curso(CursoEsperado.Nome, CursoEsperado.CargaHoraria, CursoEsperado.PublicoAlvo, CursoEsperado.Valor);
+
+            Assert.Equal(CursoEsperado.Nome, curso.Nome);
+        }
+
+        [Fact]
+        public void Curso_CriarCurso_RetornaCurso_Red()
+        {
+            var CursoEsperado = new
+            {
+                Nome = "Curso de TDD",
+                CargaHoraria = (double)40,
+                PublicoAlvo = PublicAlvo.Empresarial,
+                Valor = (decimal)100
+            };
+
+            var curso = new Curso.Curso(CursoEsperado.Nome, CursoEsperado.CargaHoraria, CursoEsperado.PublicoAlvo, CursoEsperado.Valor);
+
+            var cursoAlterado = "Curso na pratica com TDD";
+
+            Assert.Equal(cursoAlterado, curso.Nome);
+        }
+    }
+}
---
+++ CursoOnline.Domain/Curso/Curso.cs
@@ -0,0 +1,21 @@
+﻿using CursoOnline.Domain.Curso.Enum;
+
+namespace CursoOnline.Domain.Curso
+{
+    public class Curso
+    {
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public PublicAlvo PublicoAlvo { get; set; }
+        public decimal Valor { get; set; }
+
+        public Curso(string nome, double cargaHoraria, PublicAlvo publicoAlvo, decimal valor)
+        {
+
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+    }
+}
